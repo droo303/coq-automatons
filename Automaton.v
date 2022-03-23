d1a4913d718d1b8295e4571cd9db8c4b5774dc27
Petr Kožušník <petr.kozusnik@compacer.com>
2022-03-23 14:07:30 +0100

    03/23

--- Automaton.v
+++ Automaton.v
@@ -3,6 +3,7 @@ From automatons Require Export BaseDefinitions.
 Require Import Notations.
 Require Import Ltac.
 Require Import Logic.
+Require Import Bool.
 
 (* Q      - a finite set of states,
  * Sigma  - a finite set of input symbols 
@@ -87,12 +88,20 @@ Variable Automaton_b: DFA Sigma.
 
 Let Q_a := Automaton_a.(dfa_states Sigma).
 Let Q_b := Automaton_b.(dfa_states Sigma).
-Let Q := pair Q_a Q_b.
+Let Q := Q_a * Q_b.
 
 Let delta_a := Automaton_a.(dfa_trans Sigma). 
 Let delta_b := Automaton_b.(dfa_trans Sigma).
 
-(* Let delta := delta_a (fst Q) -> delta_b (snd Q) -> Q. *)
+
+Definition delta_union (q: Q)(a: Sigma) : Q :=
+  let q1 := fst q in
+  let q2 := snd q in
+  let q1':= delta_a q1 a in
+  let q2':= delta_b q2 a in
+    pair q1' q2'.
+
+Print delta_union.    
 
 Let q0_a := Automaton_a.(dfa_q0 Sigma).
 Let q0_b := Automaton_b.(dfa_q0 Sigma).
@@ -100,43 +109,22 @@ Let q0 := pair q0_a q0_b.
 
 Let qend_a := Automaton_a.(dfa_end Sigma).
 Let qend_b := Automaton_b.(dfa_end Sigma).
-(* Let qend := qend_a (fst Q) \/ qend_b (snd Q). *)
 
-(* Definition Union_auto (a: Automaton)(b: Automaton) : Automaton :=
-  createDFA Q delta q0 qend. *)
+Definition qend (q: Q) : bool := 
+  let (q1, q2) := q in
+  orb (qend_a q1)(qend_b q2).
+
+Definition Union_auto: DFA Sigma :=
+  createDFA Sigma Q delta_union q0 qend.
+
+Definition qend_ (q: Q) : bool := 
+  let (q1, q2) := q in
+  andb (qend_a q1)(qend_b q2).
+
+Definition Intersection_auto (a: DFA Sigma)(b: DFA Sigma) : DFA Sigma :=
+  createDFA Sigma Q delta_union q0 qend_.
 
 End Union_Automaton.
-
-
-Section Intersection_Automaton.
-
-Variable Sigma: Alphabet.
-Variable Automaton_a: DFA Sigma.
-Variable Automaton_b: DFA Sigma.
-
-Let Q_a := Automaton_a.(dfa_states Sigma).
-Let Q_b := Automaton_b.(dfa_states Sigma).
-Let Q := pair Q_a Q_b.
-
-Let delta_a := Automaton_a.(dfa_trans Sigma). 
-Let delta_b := Automaton_b.(dfa_trans Sigma).
-
-(* Let delta := delta_a (fst Q) -> delta_b (snd Q) -> Q. *)
-
-Let q0_a := Automaton_a.(dfa_q0 Sigma).
-Let q0_b := Automaton_b.(dfa_q0 Sigma).
-Let q0 := pair q0_a q0_b.
-
-Let qend_a := Automaton_a.(dfa_end Sigma).
-Let qend_b := Automaton_b.(dfa_end Sigma).
-
-(* Let qend := qend_a (fst Q) /\ qend_b (snd Q). *)
-
-(* Definition Intersection_auto (a: Automaton)(b: Automaton) : Automaton :=
-  createDFA Q delta q0 qend.
- *)
-End Intersection_Automaton.
-
 
 Section Concatenation_Automaton.
 
@@ -146,9 +134,7 @@ Variable Automaton_b: DFA Sigma.
 
 Let Q_a := Automaton_a.(dfa_states Sigma).
 Let Q_b := Automaton_b.(dfa_states Sigma).
-Let Q := Q_a + Q_b.
-
-Variable delta : Q -> Sigma -> Q. 
+Let Q := Q_a * Q_b.
 
 Let q0_a := Automaton_a.(dfa_q0 Sigma).
 Let qend_b := Automaton_b.(dfa_end Sigma).
@@ -159,3 +145,19 @@ Check createDFA.
   createDFA Sigma Q delta q0_a qend_b. *)
 
 End Concatenation_Automaton.
+
+Print Union_auto.
+
+(* TODO Proof for acceptance of words for both Automatons when accepted in Union_auto *)
+
+Lemma Union_trans : forall Sigma (Automaton_a Automaton_b : DFA Sigma), 
+                  let automaton_union := Union_auto Sigma Automaton_a Automaton_b in
+                  let Q1 := Automaton_a.(dfa_states Sigma) in
+                  let Q2 := Automaton_b.(dfa_states Sigma) in
+                  forall (w: Word Sigma)(q1 q1': Q1)(q2 q2': Q2),
+                    trans Sigma automaton_union (pair q1 q2) w (pair q1' q2') <-> trans Sigma Automaton_a q1 w q1' /\ trans Sigma Automaton_b q2 w q2'.
+Proof.
+  intros Sigma Automaton_a Automaton_b automaton_union Q1 Q2 w q1 q1' q2 q2'.
+
+
+
