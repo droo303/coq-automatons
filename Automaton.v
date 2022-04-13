9d08ffc429928884aca1124d732a87707bb1bba0
Petr Kožušník <petr.kozusnik@compacer.com>
2022-04-13 11:37:48 +0200

    04/13

--- Automaton.v
+++ Automaton.v
@@ -5,6 +5,11 @@ Require Import Ltac.
 Require Import Logic.
 Require Import Bool.
 
+(* Set up notattion for Alphabet (list) *)
+Notation "x :: l" := (cons x l)(at level 60, right associativity).
+Notation "[ ]" := nil.
+Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..).
+
 (* Q      - a finite set of states,
  * Sigma  - a finite set of input symbols 
  * delta  - a transition function
@@ -12,152 +17,131 @@ Require Import Bool.
  * F      - set of accept states
 *)
 
-(* Basic Automaton Prototype *)
-Structure Automaton := createAuto { Q:Set;
-                                Sigma: Alphabet;
-                                delta: Q -> Sigma -> Q;
-                                q0: Q;
-                                F: Q -> bool}.
-
-(* Parametrized Automaton *)
+(* Deterministic Finite Automaton *)
 Structure DFA  (Sigma:Alphabet) : Type := createDFA { 
-                                dfa_states: Set;
-                                dfa_trans: dfa_states -> Sigma -> dfa_states;
-                                dfa_q0: dfa_states;
-                                dfa_end: dfa_states -> bool}.
-
-(* Set up notattion for Alphabet (list) *)
-Notation "x :: l" := (cons x l)(at level 60, right associativity).
-Notation "[ ]" := nil.
-Notation "[ x ; .. ; y ]" := (cons x .. (cons y nil) ..).
+                                Q:      Set;
+                                delta:  Q -> Sigma -> Q;
+                                q0:     Q;
+                                F:      Q -> bool
+}.
 
 (* Define transition function *)
 Section Transition.
-
 Variable Sigma: Alphabet.
 Variable Automaton: DFA Sigma.
-
-(* Transition to state by word, q - starting state *)
-Fixpoint Transition (q: Automaton.(dfa_states Sigma))(w: Word Sigma) : Automaton.(dfa_states Sigma) :=
+(* Q -> Word -> Q *)
+Fixpoint Transition (q: Q Sigma Automaton)(w: Word Sigma) : Q Sigma Automaton :=
 match w with
 | nil => q
 | h::t => 
-        let q':= Automaton.(dfa_trans Sigma) q h 
+        let q':= delta Sigma Automaton q h 
         in  Transition q' t
 end.
 
+(* Inductive proposition *)
+Let Q:= Q Sigma Automaton.
+Let delta:= delta Sigma Automaton.
+Variable h t: Word Sigma.
+Inductive transition: Q -> Word Sigma -> Q -> Prop :=
+|trans_empty: forall q, transition q nil q
+|trans_ind : forall q q' q'' h t, delta q h = q' -> transition q' t q'' -> transition q (h::t) q''
+.
+
+Lemma transition_nil: forall q1 q2, transition q1 nil q2 <-> q1 = q2.
+Proof.
+  intros. split.
+  - intros. inversion H. reflexivity.
+  - intros. inversion H. apply trans_empty.
+Qed.
+
+Lemma transition_intermediate: forall q1 h t q2, 
+      transition q1 (h::t) q2 <-> exists q3, delta q1 h = q3 /\ transition q3 t q2.
+Proof.
+ intros. split.
+  - intros. inversion H. 
+    + rewrite H3. exists q'. split. 
+      * reflexivity.
+      * apply H5.
+  -
+Admitted. 
 End Transition.
 
 (* Define Accepts word function *)
 Section Accept.
-
 Variable Sigma: Alphabet.
 Variable Automaton: DFA Sigma.
+Variable word: Word Sigma.
+Let q0 := q0 Sigma Automaton.
+Let q' := Transition Sigma Automaton q0 word.
 
-Definition Accepts_word (w: Word Sigma) : bool :=
-let q0 := dfa_q0 Sigma Automaton in
-  let q' := Transition Sigma Automaton q0 w
-  in Automaton.(dfa_end Sigma) q'.
-
+Definition Accepts_word : bool :=
+  F Sigma Automaton q'.
 End Accept.
 
-Print Accepts_word.
-(* Define Transition Inductively *)
-Section Transition_Inductive.
 
-Variable Sigma: Alphabet.
-Variable Automaton: DFA Sigma.
-Variable h t: Word Sigma.
-
-Let Q:= Automaton.(dfa_states Sigma).
-Let delta:= Automaton.(dfa_trans Sigma).
-
-Inductive trans: Q -> Word Sigma -> Q -> Prop :=
-|t_a : forall q, trans q nil q
-|t_b : forall q q' q'' h t, delta q h = q' -> trans q' t q'' -> trans q (h::t) q''
-.
-
-End Transition_Inductive.
-
-
-Section Union_Automaton.
-
+Section Automaton_Operations.
 Variable Sigma: Alphabet.
 Variable Automaton_a: DFA Sigma.
 Variable Automaton_b: DFA Sigma.
 
-Let Q_a := Automaton_a.(dfa_states Sigma).
-Let Q_b := Automaton_b.(dfa_states Sigma).
+Let Q_a := Q Sigma Automaton_a.
+Let Q_b := Q Sigma Automaton_b.
 Let Q := Q_a * Q_b.
 
-Let delta_a := Automaton_a.(dfa_trans Sigma). 
-Let delta_b := Automaton_b.(dfa_trans Sigma).
+Let delta_a := delta Sigma Automaton_a. 
+Let delta_b := delta Sigma Automaton_b.
 
+Let q0_a := q0 Sigma Automaton_a.
+Let q0_b := q0 Sigma Automaton_b.
+Let q0 := pair q0_a q0_b.
+
+Let qend_a := F Sigma Automaton_a.
+Let qend_b := F Sigma Automaton_b.
 
 Definition delta_union (q: Q)(a: Sigma) : Q :=
-  let q1 := fst q in
-  let q2 := snd q in
+  let (q1, q2) := q in
   let q1':= delta_a q1 a in
   let q2':= delta_b q2 a in
     pair q1' q2'.
 
-Print delta_union.    
-
-Let q0_a := Automaton_a.(dfa_q0 Sigma).
-Let q0_b := Automaton_b.(dfa_q0 Sigma).
-Let q0 := pair q0_a q0_b.
-
-Let qend_a := Automaton_a.(dfa_end Sigma).
-Let qend_b := Automaton_b.(dfa_end Sigma).
-
-Definition qend (q: Q) : bool := 
+Definition qend_union (q: Q) : bool := 
   let (q1, q2) := q in
   orb (qend_a q1)(qend_b q2).
 
-Definition Union_auto: DFA Sigma :=
-  createDFA Sigma Q delta_union q0 qend.
-
-Definition qend_ (q: Q) : bool := 
+Definition qend_intersection (q: Q) : bool := 
   let (q1, q2) := q in
   andb (qend_a q1)(qend_b q2).
 
-Definition Intersection_auto (a: DFA Sigma)(b: DFA Sigma) : DFA Sigma :=
-  createDFA Sigma Q delta_union q0 qend_.
+Definition Union_automata: DFA Sigma :=
+  createDFA Sigma Q delta_union q0 qend_union.
 
-End Union_Automaton.
+Definition Intersection_automata (a: DFA Sigma)(b: DFA Sigma) : DFA Sigma :=
+  createDFA Sigma Q delta_union q0 qend_intersection.
 
-Section Concatenation_Automaton.
+(* Add concatenation *)
 
-Variable Sigma: Alphabet.
-Variable Automaton_a: DFA Sigma.
-Variable Automaton_b: DFA Sigma.
+(* Add complement *)
 
-Let Q_a := Automaton_a.(dfa_states Sigma).
-Let Q_b := Automaton_b.(dfa_states Sigma).
-Let Q := Q_a * Q_b.
-
-Let q0_a := Automaton_a.(dfa_q0 Sigma).
-Let qend_b := Automaton_b.(dfa_end Sigma).
-
-Check createDFA.
-
-(* Definition Concatenation_auto : DFA Sigma :=
-  createDFA Sigma Q delta q0_a qend_b. *)
-
-End Concatenation_Automaton.
-
-Print Union_auto.
+End Automaton_Operations.
 
 (* TODO Proof for acceptance of words for both Automatons when accepted in Union_auto *)
+Section proof.
 
 Lemma Union_trans : forall Sigma (Automaton_a Automaton_b : DFA Sigma), 
-                  let automaton_union := Union_auto Sigma Automaton_a Automaton_b in
-                  let Q1 := Automaton_a.(dfa_states Sigma) in
-                  let Q2 := Automaton_b.(dfa_states Sigma) in
+                  let automaton_union := Union_automata Sigma Automaton_a Automaton_b in
+                  let Q1 := Q Sigma Automaton_a in
+                  let Q2 := Q Sigma Automaton_b in
                   forall (w: Word Sigma)(q1 q1': Q1)(q2 q2': Q2),
-                    trans Sigma automaton_union (pair q1 q2) w (pair q1' q2') <-> trans Sigma Automaton_a q1 w q1' /\ trans Sigma Automaton_b q2 w q2'.
+                    transition Sigma automaton_union (pair q1 q2) w (pair q1' q2') <-> (transition Sigma Automaton_a q1 w q1' /\ transition Sigma Automaton_b q2 w q2').
+
 Proof.
-  intros Sigma Automaton_a Automaton_b automaton_union Q1 Q2 w q1 q1' q2 q2'.
-
-
-
+  intros Sigma Automaton_a Automaton_b automaton_union Q1 Q2.
+  induction w.
+    - intros. split. 
+      + intros. apply transition_nil in H. inversion H. split.
+        apply trans_empty. apply trans_empty. 
+      + intros. destruct H as [H1 H2]. apply transition_nil in H1. apply transition_nil in H2. rewrite H1. rewrite H2.
+      apply trans_empty. 
+    - intros. split.
+      + intros H. apply transition_intermediate in H.
+Admitted.
